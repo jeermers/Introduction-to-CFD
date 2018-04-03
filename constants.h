919f1325112230186eb518646c80c7c7dd2db777
Scholten <f.t.e.scholten@student.tue.nl>
2018-04-03 15:04:47 +0200

    Baffle added V3

--- constants.h
+++ constants.h
@@ -1,5 +1,5 @@
-#define NPI        200        /* number of grid cells in x-direction [-] */
-#define NPJ        80        /* number of grid cells in y-direction [-] */
+#define NPI        300        /* number of grid cells in x-direction [-] */
+#define NPJ        120        /* number of grid cells in y-direction [-] */
 #define XMAX       10.0      /* width of the domain [m] */ 
 #define YMAX       0.2       /* height of the domain [m] */
 #define PI         3.1415927 /* value of pi [-] */
@@ -16,7 +16,7 @@
 #define LARGE      1E30      /* arbitrary very large value [-] */
 #define SMALL      1E-30     /* arbitrary very small value [-] */
 #define P_ATM      101000.   /* athmospheric pressure [Pa] */
-#define U_IN       0.5       /* in flow velocity [m/s] */
+#define U_IN       0.1       /* in flow velocity [m/s] */
 #define frac_lower 0.0       /* fraction in lower part of the channel */
 #define frac_upper 1.0       /* fraction in upper part of the channel */
 
